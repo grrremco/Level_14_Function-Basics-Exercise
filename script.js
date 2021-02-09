0ac21e88a5ab69d7444b3eb215642461c43398d8
Remco van de Siepkamp <69791872+grrremco@users.noreply.github.com>
2021-02-09 16:43:38 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const makePizza = function() {
+    console.log('Make the pizza dough');
+    console.log('Leave the dough to rest')
+    console.log('Make the sauce');
+    console.log('Role out the dough');
+    console.log('Spread the sauce evenly over the dough');
+    console.log('Throw cheese on the pizza');
+    console.log('Put some toppings on the pizza');
+    console.log('Bake the pizza at 220 degrees Celsius for 15 minutes');
+};
+
+const makeSushi = function() {
+    console.log('Boil rice and let it cool');
+    console.log('Rinse the rice');
+    console.log('Cut all the ingrediënts to prevered measurements');
+    console.log('Lay down your bamboo mat');
+    console.log('Put the Nori on the mat and cover it with rice');
+    console.log('Add the other ingrediënts in a horizontal line at the front of the mat');
+    console.log('Now make a role using the bamboo mat, press firmly');
+    console.log('Take of the bamboo mat and slice the role into 4cm plaques');
+    console.log('Finaly throw everything away, and get a patatje oorlog!');
+};
+
+makePizza();
+makePizza();
+
+makeSushi();
+makeSushi();
+makeSushi();
